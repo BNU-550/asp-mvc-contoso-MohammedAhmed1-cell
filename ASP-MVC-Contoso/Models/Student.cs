6acedff93f9104d803debe6976479fd2e9505145
Mohammed Ahmed <reazssj4@gmail.com>
2021-11-01 12:44:58 +0000

    Updated

---
+++ ASP-MVC-Contoso/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ASP_MVC_Contoso.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        // Relationships or navigation properties
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
--- ASP-MVC-Contoso/Startup.cs
+++ ASP-MVC-Contoso/Startup.cs
@@ -30,6 +30,7 @@ namespace ASP_MVC_Contoso
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
